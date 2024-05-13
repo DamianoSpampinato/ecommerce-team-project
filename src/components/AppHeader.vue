ba84ee391e1e2231828f4bc0ae80d3ef4bc44cc4
gianlucafalaschi <gian100gian@outlook.it>
2024-05-13 18:38:24 +0200

    header top left center and right saparated

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,36 +5,46 @@
     <header>
         <div class="ms-header-container">
             <div class="ms-row d-flex align-items-center py-2">
-                <div class="ms-logo-container mx-2">
+                <!-- header top left -->
+                <div class="ms-header-top-left d-flex">
+                    <div class="ms-logo-container mx-2">
                     <img class="img-fluid"
                         src="https://cdn.vectorstock.com/i/500p/74/63/amazon-logo-background-icon-vector-50147463.jpg"
                         alt="">
-                </div>
-                <div class="ms-send-to mx-2">
-                    <div>Invia a</div>
-                    <div class="text-white"><i class="fa-solid fa-location-dot"></i></div>
-                </div>
-                <div class="ms-input-container mx-2">
-                    <div class="input-group mb-3">
-                        <button class="btn btn-outline-secondary dropdown-toggle" type="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Separated link</a></li>
-                        </ul>
-                        <input type="text" class="form-control" aria-label="Text input with dropdown button">
+                    </div>
+                    <div class="ms-send-to mx-2">
+                        <div>Invia a</div>
+                        <div class="text-white"><i class="fa-solid fa-location-dot"></i></div>
                     </div>
                 </div>
+                <!--/ header top left -->
+                <!-- header top center -->
+                <div class="ms-header-top-center mx-3">
+                    <div class="ms-input-container mx-2 ">
+                        <div class="input-group mb-3">
+                            <button class="btn btn-outline-secondary dropdown-toggle" type="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Separated link</a></li>
+                            </ul>
+                            <input type="text" class="form-control" aria-label="Text input with dropdown button">
+                        </div>
+                    </div>
+                </div>
+                <!--/ header top center -->
+                <!-- header top-right -->
                 <div class="ms-header-top-right d-flex ms-auto justify-content-end gap-3">
                     <div class="">Account e liste</div>
                     <div class="">Resi e ordini</div>
                     <div>Carrello</div>
                 </div>
+                <!--/ header top-right -->
             </div>
         </div>
     </header>
@@ -47,11 +57,18 @@
     background-color: coral;
 }
 
-.ms-logo-container {
+.ms-header-top-left {
     width: 15%;
 }
 
-.ms-input-container {
+/* .ms-logo-container {
+    width: 15%;
+} */
+.ms-header-top-center{
+    width: 60%;
+}
 
+.ms-header-top-right {
+    width: 15%;
 }
 </style>
