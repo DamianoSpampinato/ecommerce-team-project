babd1459d9a0e3d5d0a32b369546fd0aff90a5fa
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-05-13 17:00:03 +0200

    feat: add AppMain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,22 @@
+<script>
+    export default{
+        components:{
+            
+        },
+        data(){
+            return{
+
+            }
+        },
+    }
+</script>
+
+<template>
+    <main>
+
+    </main>
+</template>
+
+<style lang="scss" scoped>
+    @use '../style/partials/variables' as *;
+</style>
